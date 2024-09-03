8abaf05b150b41ff65364249dd2014e26fe47b37
sunilrpandey <inkingmind@gmail.com>
2024-09-03 19:47:56 +0530

    temp checkin

---
+++ 05_google_test_fetch_content/mathproj.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "mathlib.h"
+
+int main() {
+    std::cout << "Hello Math!" << std::endl;
+
+    std::cout << "Sum of 3 and 5 : " << sum(3,5) << std::endl;
+    return 0;
+}
