3446acc0de176b654a25a2420c858399c5022c3e
sunilrpandey <inkingmind@gmail.com>
2024-09-07 00:40:22 +0530

    changes made to add submodule

--- 05_use_external_lib_as_git_submodule/mathproj.cpp
+++ 05_use_external_lib_as_git_submodule/mathproj.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "mathlib.h"
-//#include <nlohmann/json.hpp>
+#include <nlohmann/json.hpp>
 
+#include "mathlib.h"
 
 int main() {
     std::cout << "Hello Math!" << std::endl;
 
-    // std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "."
-    //           << NLOHMANN_JSON_VERSION_PATCH << "\n";
+     std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "."
+              << NLOHMANN_JSON_VERSION_PATCH << "\n";
 
     std::cout << "Sum of 3 and 5 : " << sum(3,5) << std::endl;
     return 0;
