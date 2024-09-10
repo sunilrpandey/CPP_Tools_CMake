c85864bf6b71788469ebe903f2d4620cd282bce3
sunilrpandey <inkingmind@gmail.com>
2024-09-10 14:30:24 +0530

    added based project to demo add compile options

---
+++ 07_add_compile_options/07_add_compile_options.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "fun_lib1.h"
+#include "fun_lib2.h"
+
+int main()
+{
+    std::cout << "Demo : add compile options!" << std::endl;
+    printMessage1();
+    printMessage2();
+    return 0;
+}
