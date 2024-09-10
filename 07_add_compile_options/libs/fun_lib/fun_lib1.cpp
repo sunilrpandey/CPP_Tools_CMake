3d48e6d916207a01c14dcff8d7a7bda94224bf9f
sunilrpandey <inkingmind@gmail.com>
2024-09-10 14:30:24 +0530

    added based project to demo add compile options

---
+++ 07_add_compile_options/libs/fun_lib/fun_lib1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "fun_lib1.h"
+
+/**
+ * @brief Print Message for lib1
+ *
+ */
+
+void printMessage1(){
+
+    std::cout << "Messaging from lib1!!" << std::endl;
+    return;
+}
